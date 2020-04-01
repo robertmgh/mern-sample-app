29cd00a389a2c74fbaa0f43d2e25bd6d4a9c2db5
Michnowski, Robert <robert.michnowski@accenture.com>
2020-04-02 01:03:32 +0200

    Fix app

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
-import { AppBar, ListItem, ListItemIcon, ListItemText, List, Divider, Drawer, Toolbar } from '@material-ui/core';
+import { Grid, AppBar, ListItem, ListItemIcon, ListItemText, List, Toolbar, Button } from '@material-ui/core';
 import InboxIcon from '@material-ui/icons/Inbox';
 import AppRouter from './appRouter/AppRouter';
 import { Link } from 'react-router-dom';
@@ -10,7 +10,7 @@ const drawerWidth = 240;
 
 const useStyles = makeStyles(theme => ({
     root: {
-        display: 'flex'
+        flexGrow: 1
     },
     drawer: {
         width: drawerWidth,
@@ -31,36 +31,33 @@ function App() {
     const classes = useStyles();
 
     return (
-        <div className={classes.root}>
-            <BrowserRouter basename="/">
-                <AppBar position="fixed">
-                    <Toolbar></Toolbar>
-                </AppBar>
-                <Drawer
-                    className={classes.drawer}
-                    variant="permanent"
-                    classes={{
-                        paper: classes.drawerPaper
-                    }}
-                    anchor="left"
-                >
-                    <div className={classes.toolbar} />
-                    <Divider />
-                    <List>
-                        <ListItem button component={Link} to="/">
-                            <ListItemIcon>
-                                <InboxIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="List" />
-                        </ListItem>
-                    </List>
-                </Drawer>
-                <main className={classes.content}>
-                    <div className={classes.toolbar} />
-                    <AppRouter />
-                </main>
-            </BrowserRouter>
-        </div>
+        <BrowserRouter basename="/">
+            <AppBar position="fixed">
+                <Toolbar>
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
+            <main className={classes.content}>
+                <div className={classes.toolbar} />
+                <Grid container direction="column" spacing={3} className={classes.root}>
+                    <Grid container item direction="row" xs={12}>
+                        <Grid item style={{ width: 240 }}>
+                            <List>
+                                <ListItem button component={Link} to="/">
+                                    <ListItemIcon>
+                                        <InboxIcon />
+                                    </ListItemIcon>
+                                    <ListItemText primary="List" />
+                                </ListItem>
+                            </List>
+                        </Grid>
+                        <Grid item xs="auto">
+                            <AppRouter />
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </main>
+        </BrowserRouter>
     );
 }
 
